c99aa9e64a0711bbff17817624331e0e0353ccaa
bluefuton <chris@bluefuton.com>
2015-12-18 12:34:08 +1100

    Refactor to use a parent ListManagement component which fetches list details, and hands it down to each tab

--- client/reader/list-management/tags/index.jsx
+++ client/reader/list-management/tags/index.jsx
@@ -4,8 +4,6 @@ import times from 'lodash/utility/times';
 import debugModule from 'debug';
 
 // Internal dependencies
-import Main from 'components/main';
-import Navigation from 'reader/list-management/navigation';
 import InfiniteList from 'components/infinite-list';
 import ListItem from 'reader/list-item';
 import Icon from 'reader/list-item/icon';
@@ -14,14 +12,13 @@ import Description from 'reader/list-item/description';
 import Actions from 'reader/list-item/actions';
 import Gridicon from 'components/gridicon';
 import ListStore from 'lib/reader-lists/lists';
-import ReaderListsStore from 'lib/reader-lists/subscriptions';
-import ReaderListsActions from 'lib/reader-lists/actions';
 import ReaderListsTagsStore from 'lib/reader-lists-tags/store';
 import { fetchMoreTags } from 'lib/reader-lists-tags/actions';
 import smartSetState from 'lib/react-smart-set-state';
 import ListManagementError from '../error';
 import EmptyContent from 'components/empty-content';
 
+const stats = require( 'reader/stats' );
 const debug = debugModule( 'calypso:reader:list-management' ); // eslint-disable-line
 
 const ListManagementTags = React.createClass( {
@@ -39,13 +36,8 @@ const ListManagementTags = React.createClass( {
 	},
 
 	getStateFromStores() {
-		// Grab the list ID from the list store
-		const list = ListStore.get( this.props.list.owner, this.props.list.slug );
-		if ( ! list && ! ReaderListsStore.isFetching() ) {
-			ReaderListsActions.fetchList( this.props.list.owner, this.props.list.slug );
-		}
-
 		// Fetch tags, but only if we have the list information
+		const list = this.props.list;
 		let tags = null;
 		let isLastPage = false;
 		let currentPage = 0;
@@ -56,7 +48,6 @@ const ListManagementTags = React.createClass( {
 		}
 
 		return {
-			list,
 			tags,
 			isLastPage,
 			currentPage,
@@ -134,7 +125,7 @@ const ListManagementTags = React.createClass( {
 			return null;
 		}
 
-		if ( this.state.list && this.state.tags.length === 0 && this.state.isLastPage ) {
+		if ( this.props.list && this.state.tags.length === 0 && this.state.isLastPage ) {
 			return ( <EmptyContent
 						title={ this.translate( 'This list does not have any tags yet.' ) }
 						illustration={ '/calypso/images/drake/drake-404.svg' }
@@ -157,22 +148,21 @@ const ListManagementTags = React.createClass( {
 	},
 
 	render() {
-		if ( ! this.state.list && this.state.lastListError ) {
+		if ( ! this.props.list && this.state.lastListError ) {
 			return ( <ListManagementError /> );
 		}
 
 		let message = null;
-		if ( ! this.state.list ) {
+		if ( ! this.props.list ) {
 			message = ( <p> {this.translate( 'Loading list information…' ) } </p> );
 		}
 
 		return (
-			<Main className="list-management-tags">
-				<Navigation selected="tags" list={ this.props.list } />
+			<div>
 				{ message }
 				{ this.renderTagList() }
-			</Main>
-			);
+			</div>
+		);
 	}
 } );
 
