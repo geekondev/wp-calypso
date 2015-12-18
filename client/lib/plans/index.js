5fe11940b05550a485de2ee69f351b487aa66061
Drew Blaisdell <drew.blaisdell@gmail.com>
2015-12-17 16:33:34 -0800

    Free Trials: Add expired state to `PlanStatusProgress`

--- client/lib/plans/index.js
+++ client/lib/plans/index.js
@@ -8,3 +8,17 @@ export function getDaysUntilUserFacingExpiry( plan ) {
 
 	return userFacingExpiryMoment.diff( moment(), 'days' );
 };
+
+export function getDaysUntilExpiry( plan ) {
+	const { expiryMoment } = plan;
+
+	return expiryMoment.diff( moment(), 'days' );
+};
+
+export function isInGracePeriod( plan ) {
+	if ( getDaysUntilUserFacingExpiry( plan ) <= 0 && getDaysUntilExpiry( plan ) > 0 ) {
+		return true;
+	}
+
+	return false;
+};
